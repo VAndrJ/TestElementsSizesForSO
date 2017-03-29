ce64a9f7dbe6caa8431dac86b3c1a9d9b50ba2c3
MacMini <macmini@MacMinis-Mac-mini.local>
2017-03-29 11:44:36 +0300

    Initial Commit

---
+++ TestElementsSizesForSO/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TestElementsSizesForSO
+//
+//  Created by MacMini on 3/29/17.
+//  Copyright © 2017 VAndrJ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ TestElementsSizesForSO/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  TestElementsSizesForSO
+//
+//  Created by MacMini on 3/29/17.
+//  Copyright © 2017 VAndrJ. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
